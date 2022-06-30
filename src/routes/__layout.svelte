d8daf213a21cfe07cb8df4fa2769bab27dd1898b
Fabio Plunser <f.plunser@outlook.com>
2022-06-30 14:08:52 +0200

    i think i added dark mode quiet buggy. Self hosted Portfolio webiste as a docker Container with SvelteKit als full stack framework

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,45 +1,21 @@
-<script lang="ts">
-	import Header from '$lib/header/Header.svelte';
-	import '../app.css';
+
+<script>
+    let dark = true;
+    import Index from "./index.svelte"
 </script>
 
-<header />
+<div class:dark>
+    <main class="bg-white dark:bg-gray-800 text-black dark:text-white min-h-screen">
+        <Index bind:dark/>
+        <!-- <button on:click={dark} class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Change Theme</button> -->
+        <!-- <slot/> -->
+    </main>
+</div>
 
-<main>
-	<slot />
-</main>
-
-<footer>
-	<p>visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to learn SvelteKit</p>
-</footer>
 
 <style>
-	main {
-		flex: 1;
-		display: flex;
-		flex-direction: column;
-		padding: 1rem;
-		width: 100%;
-		max-width: 1024px;
-		margin: 0 auto;
-		box-sizing: border-box;
-	}
-
-	footer {
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
-		align-items: center;
-		padding: 40px;
-	}
-
-	footer a {
-		font-weight: bold;
-	}
-
-	@media (min-width: 480px) {
-		footer {
-			padding: 40px 0;
-		}
-	}
+	@tailwind base;
+	@tailwind components;
+	@tailwind utilities;
 </style>
+  
