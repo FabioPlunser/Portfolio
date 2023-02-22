f392d7e833e1b676dce2712fdcfc1ffbba888ad6
Fabio Plunser <f.plunser@outlook.com>
2023-02-22 08:13:23 +0100

    added hero for desktop

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,9 @@
 <script lang="ts">
     import '../app.css';
     import Nav from '$components/ui/Nav.svelte';
+    
 </script>
+
 
 
 <Nav/>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+    import Test from '$lib/assets/Ich.png';
+</script>
+
+
+<section id="hero">
+    <div class="mt-20 flex justify-center">
+        <div class="flex ">
+            <div class="items-center my-auto relative">
+                <h1 class="text-5xl font-bold">Hello, </h1>
+                <h1 class="text-5xl font-bold">I'm Fabio Plunser</h1>
+                <h1 class="text-5xl font-bold">Student for computer science</h1>
+                <h1 class="text-5xl text-primary">From Innsbruck</h1>
+                <div>
+                    <button class="btn btn-primary mt-10 text-3xl">Skills</button>
+                    <button class="btn btn-primary mt-10 text-3xl">Projects</button>
+                    <button class="btn btn-primary mt-10 text-3xl">Blog</button>
+                </div>
+                <i class="bi bi-envelope text-2xl"></i> <a class="text-2xl" href="mailto:f.plunser@outlook.com">f.plunser@outlook.com</a>
+            </div>
+            <img src={Test} alt="Hero" class="max-w-4xl " />
+        </div>
+    </div>
+    
+</section>
