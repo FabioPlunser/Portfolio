65351e255914adb9af7b8e67ea156d6422cafc0e
Fabio Plunser <f.plunser@outlook.com>
2023-02-27 16:10:57 +0100

    update

---
+++ src/lib/helper/Mobile.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+    import MediaQuery from "./MediaQuery.svelte"
+</script>
+
+
+<MediaQuery query="(max-width: 580px)" let:matches>
+    {#if matches}
+        <slot />
+    {/if}
+</MediaQuery>
+
+
+
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,25 +1,48 @@
 <script lang="ts">
     import Test from '$lib/assets/Ich.png';
     import {fly} from "svelte/transition";
+    import { onMount } from 'svelte';
+    import Mobile from "$helper/Mobile.svelte"
+    import Desktop from "$helper/Desktop.svelte"
+
+    let visible = false;
+    onMount(()=>{
+        visible = true;
+    })
 </script>
 
-
-<section id="hero">
-    <div class="mt-20 flex justify-center">
-        <div class="flex">
-            <div   class="items-center my-auto relative">
-                <h1 class="text-5xl font-bold">Hello, </h1>
-                <h1 class="text-5xl font-bold">I'm Fabio Plunser</h1>
-                <h1 class="text-5xl font-bold">Student for computer science</h1>
-                <h1 class="text-5xl text-primary">From Innsbruck</h1>
-                <div>
-                    <button class="btn btn-primary mt-10 text-3xl">Skills</button>
-                    <button class="btn btn-primary mt-10 text-3xl">Projects</button>
-                    <button class="btn btn-primary mt-10 text-3xl">Blog</button>
-                </div>
-                <i class="bi bi-envelope text-2xl"></i> <a class="text-2xl" href="mailto:f.plunser@outlook.com">f.plunser@outlook.com</a>
-            </div>
-            <!-- <img src={Test} alt="Hero" class="max-w-3xl " /> -->
+{#if visible}
+<section>
+    <Mobile>
+        <div transition:fly={{x: -200, duration: 300}} class="mt-24">
+            <h1 class="flex-none text-3xl font-bold">Hello, </h1>
+            <h1 class="flex-none text-3xl font-bold">I'm Fabio Plunser</h1>
+            <h1 class="flex-none text-3xl font-bold">Student for computer science</h1>
+            <h1 class="flex-none text-2xl text-primary">From Innsbruck</h1>
         </div>
-    </div>
+        <img transition:fly={{y: 200, duration:300}} src={Test} alt="Hero" class="fixed max-w-xl"/>
+    </Mobile>
+
+    <Desktop>
+        <div class="mt-20 flex justify-center">
+            <div class="flex">
+                <div class="items-center my-auto relative">
+                    <h1 class="text-5xl font-bold">Hello, </h1>
+                    <h1 class="text-5xl font-bold">I'm Fabio Plunser</h1>
+                    <h1 class="text-5xl font-bold">Student for computer science</h1>
+                    <h1 class="text-5xl text-primary">From Innsbruck</h1>
+                    <div>
+                        <button class="btn btn-primary mt-10 text-3xl">Skills</button>
+                        <button class="btn btn-primary mt-10 text-3xl">Projects</button>
+                        <button class="btn btn-primary mt-10 text-3xl">Blog</button>
+                    </div>
+                    <i class="bi bi-envelope text-2xl"></i> <a class="text-2xl" href="mailto:f.plunser@outlook.com">f.plunser@outlook.com</a>
+                </div>
+                <img src={Test} alt="Hero" class="fixed max-w-xl" />
+            </div>
+        </div>
+    </Desktop>
+    
+  
 </section>
+{/if}
