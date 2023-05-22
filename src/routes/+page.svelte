c625b16eb10cbd3ded2e95397545c94a9e72b0f9
Fabio Plunser <fabio.plunser@student.uibk.ac.at>
2023-05-22 11:17:10 +0200

    update

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,44 +5,14 @@
     import Mobile from "$helper/Mobile.svelte"
     import Desktop from "$helper/Desktop.svelte"
     import { typewriter } from '$helper/typewriter';
-    let visible = false;
+    let rendered = false;
     onMount(()=>{
-        visible = true;
+        rendered = true;
     })
 </script>
 
-{#if visible}
+{#if rendered}
 <section>
-    <Mobile>
-        <div transition:fly={{x: -200, duration: 300}} class="mt-24">
-            <h1 class="flex-none text-3xl font-bold">Hello, </h1>
-            <h1 class="flex-none text-3xl font-bold">I'm Fabio Plunser</h1>
-            <h1 class="flex-none text-3xl font-bold">Student for computer science</h1>
-            <h1 class="flex-none text-2xl text-primary" transition:typewriter>From Innsbruck</h1>
-        </div>
-        <img transition:fly={{y: 200, duration:300}} src={Test} alt="Hero" class="fixed max-w-xl"/>
-    </Mobile>
-
-    <Desktop>
-        <div class="mt-20 flex justify-center">
-            <div class="flex">
-                <div class="items-center my-auto relative">
-                    <h1 class="text-5xl font-bold">Hello, </h1>
-                    <h1 class="text-5xl font-bold">I'm Fabio Plunser</h1>
-                    <h1 class="text-5xl font-bold">Student for computer science</h1>
-                    <h1 class="text-5xl text-primary">From Innsbruck</h1>
-                    <div>
-                        <button class="btn btn-primary mt-10 text-3xl">Skills</button>
-                        <button class="btn btn-primary mt-10 text-3xl">Projects</button>
-                        <button class="btn btn-primary mt-10 text-3xl">Blog</button>
-                    </div>
-                    <i class="bi bi-envelope text-2xl"></i> <a class="text-2xl" href="mailto:f.plunser@outlook.com">f.plunser@outlook.com</a>
-                </div>
-                <img src={Test} alt="Hero" class="max-w-xl" />
-            </div>
-        </div>
-    </Desktop>
-    
-  
+ 
 </section>
 {/if}
