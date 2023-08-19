7a1fc68400ef427c42bde4f1f665461b7adb5ce2
Fabio Plunser <f.plunser@outlook.com>
2023-08-19 16:44:30 +0200

    added best projects section with project cards, animation with svelte-motiony

---
+++ src/lib/components/ProjectCard.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+	import tilt from '$lib/animations/tilt';
+</script>
+
+<div
+	use:tilt={{ max: 7, reverse: true }}
+	class="card shadow-xl rounded-3xl h-96 backdrop-blur-2xl bg-gray-800/70"
+>
+	<img
+		src="https://picsum.photos/700/300"
+		alt=""
+		class="rounded-3xl m-2 shadow-2xl backdrop-blur-2xl"
+	/>
+	<div class=" ml-4">
+		<h1 class="card-title font-bold">Project</h1>
+		<p>Describing the project in a few words</p>
+	</div>
+	<div class="absolute bottom-0 right-0 mr-4 mb-4">
+		<button class="btn btn-accent rounded-2xl font-bold text-white text-xl">Detail</button>
+	</div>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,38 +1,81 @@
 <script lang="ts">
-	import HeroBackground from '$lib/images/yellow-bg.svg.svelte';
-	import HeroImage from '$lib/images/me_denoised.png';
-	import Testimonial from '$lib/components/Testimonial.svelte';
+	import Motion from 'svelte-motion/src/motion/MotionSSR.svelte';
+	import { fly, fade } from 'svelte/transition';
+	import ProjectCard from '$lib/components/ProjectCard.svelte';
+	import { onMount } from 'svelte';
+	import { inview } from 'svelte-inview';
+
+	let isInView = false;
+	let pageLoad = false;
+	onMount(() => {
+		pageLoad = true;
+	});
+	$: console.log(isInView);
+
+	const spring = {
+		type: 'spring',
+		stiffness: 700,
+		damping: 30
+	};
 </script>
 
-<section class="mt-24 mx-4">
-	<div class="flex justify-center font-bold w-64 text-3xl relative z-10">
-		<p>
-			Hi, I'm Fabio Plunser a computer science student 
-            <!-- with a passion for
-			<span class="text-primary">software,</span>
-			<span class="text-accent">hardware</span> and
-			<span class="text-secondary">network</span> -->
-		</p>
-	</div>
+<Motion
+	initial={{ y: -600 }}
+	animate={{ y: 0 }}
+	transition={{ ype: 'fly', duration: 0.5 }}
+	let:motion
+>
+	<section id="Hero" class="mt-32 mx-16">
+		<div use:motion class="flex justify-center font-bold w-1/3 text-5xl relative z-10">
+			<p>
+				Hi, <br /> <br /> I'm Fabio Plunser a computer science student with a passion for
+				<span class="text-primary">software,</span>
+				<span class="text-accent">hardware</span> and
+				<span class="text-secondary">network</span>
+			</p>
+		</div>
+	</section>
+</Motion>
 
-	<button class="btn mt-4 rounded-2xl font-bold text-2xl text-white">Contact</button>
-	<div class="relative overflow-hidden">
-		<div class="absolute top-0 left-0 w-full h-full">
-			<HeroBackground />
+<div
+	use:inview={{ unobserveOnEnter: true, rootMargin: '-30%' }}
+	on:inview_change={(event) => {
+		isInView = event.detail.inView;
+	}}
+	id="Projects"
+	class="mx-16 mt-32"
+>
+	{#if isInView}
+		<div class="flex justiy-center mx-auto">
+			<Motion
+				initial={{ y: -50, opacity: 0 }}
+				animate={{ y: 0, opacity: 1 }}
+				transition={{ type: 'spring', duration: 1.25, delay: 0.25 }}
+				let:motion
+			>
+				<div use:motion class="mx-auto">
+					<h1 class="text-5xl font-bold flex justify-center mb-6">Best Projects</h1>
+
+					<div class="relative w-full grid grid-cols-2 gap-8 justify-center mx-auto">
+						{#each [1, 2, 3, 4] as i}
+							<Motion
+								initial={{ x: -100, opacity: 0 }}
+								animate={{ x: 0, opacity: 1 }}
+								transition={{ type: 'spring', duration: 1.25, delay: 0.25 * i }}
+								let:motion
+							>
+								<div use:motion>
+									<ProjectCard />
+								</div>
+							</Motion>
+						{/each}
+					</div>
+
+					<div class="flex justify-center mt-8">
+						<a href="/projects" class="btn btn-primary text-3xl text-white rounded-2xl">View All</a>
+					</div>
+				</div>
+			</Motion>
 		</div>
-	</div>
-	<!-- ------------------------------------------------------------------- -->
-	<br />
-	<h1 class="text-3xl font-bold underline flex justify-center">Best Project</h1>
-	<div class="flex justify-center mt-4">
-		<div class="card w-full bg-slate-800 shadow-xl mx-4">
-			<div class="rounded-2xl flex justify-center p-4">
-				<img src="https://picsum.photos/400/400" alt="Placeholder" class="rounded-2xl" />
-			</div>
-		</div>
-	</div>
-	<!--  -->
-	<br />
-	<h1 class="text-3xl font-bold underline flex justify-center">Testimonials</h1>
-	<Testimonial />
-</section>
+	{/if}
+</div>
