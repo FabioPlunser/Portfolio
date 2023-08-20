87ed2412e653d7fd3fb43b34218bc3d956ca7ea5
Fabio Plunser <f.plunser@outlook.com>
2023-08-20 20:55:17 +0200

    update

--- src/lib/components/hero/VerticalTimeLine.svelte
+++ src/lib/components/hero/VerticalTimeLine.svelte
@@ -4,7 +4,7 @@
 			date: '2023-08-01',
 			company: 'Company 1',
 			title: 'React.js Developer',
-			description: 'Making something nice',
+			description: "Making something niceaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa",
 			logo: 'https://upload.wikimedia.org/wikipedia/commons/thumb/6/6f/Logo_of_Twitter.svg/934px-Logo_of_Twitter.svg.png'
 		},
 		{
@@ -28,7 +28,7 @@
 <section class="mt-12 relative">
 	<div class="mt-20 flex flex-col">
 		{#each timelineItems as item}
-			<div class="relative w-full m-auto p-4">
+			<div class="relative w-full m-auto p-4 ">
 				<div class="absolute top-0 h-full w-1 bg-white">
 					<div class="relative m-2">
 						<span class="absolute top-0 -left-[25px] w-[40px] h-[40px] rounded-full bg-white">
@@ -38,13 +38,13 @@
 						</span>
 					</div>
 				</div>
-				<div class="relative ml-[30px] rounded-xl bg-slate-800 p-4">
+				<div class="relative ml-[30px] rounded-xl bg-slate-800 p-4 animate__animated animate__bounceInRight">
 					<h1 class="text-2xl font-bold flex justify-start">{item.title}</h1>
 					<div class="flex gap-4 text-gray-500">
 						<span>{item.company}</span>
 						<span>{item.date}</span>
 					</div>
-					<p>
+					<p class="w-full break-all">
 						{item.description}
 					</p>
 				</div>
