c51c51cf5ae359f4f7531e7a34e621c306fff640
Fabio Plunser <f.plunser@outlook.com>
2023-08-22 06:22:48 +0200

    added markdown for experience timeliney

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -7,7 +7,40 @@ const config = {
     content: ["./src/**/*.{html,js,svelte,ts}"],
 
     theme: {
-        extend: {}
+        extend: {
+            typography: (theme) => ({
+                "custom": {
+                    css: {
+                        color: theme("colors.white"),
+                        a: {
+                            color: theme("colors.gray.700"),
+                        },
+                        h1: {
+                            color: theme("colors.white"),
+                        },
+                        h2: {
+                            color: theme("colors.white"),
+                        },
+                        h3: {
+                            color: theme("colors.white"),
+                        },
+                        h4: {
+                            color: theme("colors.white"),
+                        },
+                        h5: {
+                            color: theme("colors.white"),
+                        },
+                        ul: {
+                            color: theme("colors.white"),
+                            li: {
+                                color: theme("colors.white"),
+                            }
+                        },
+                    }
+                }
+            }),
+        },
+
     },
 
     plugins: [forms, typography, daisyui],
