5c5240126427a5c26e40e6316f6163770699f8b2
Fabio Plunser <fabio.plunser@student.uibk.ac.at>
2023-10-09 21:12:21 +0200

    fixed vertical timeline animation for desktop need to add to mobile version

--- src/lib/components/hero/VerticalTimeLine.svelte
+++ src/lib/components/hero/VerticalTimeLine.svelte
@@ -1,6 +1,9 @@
 <script lang="ts">
 	import { marked } from 'marked';
-	import { fly } from 'svelte/transition';
+	import { fly, scale } from 'svelte/transition';
+	import { quintIn, quintInOut, cubicInOut, bounceIn } from 'svelte/easing';
+	import { inview } from 'svelte-inview';
+
 	let timelineItems = [
 		{
 			id: 1,
@@ -49,8 +52,6 @@
 		}
 	];
 
-	import { inview } from 'svelte-inview';
-
 	const options = {
 		rootMargin: '-50px'
 		// unobserveOnEnter: true
@@ -76,7 +77,100 @@
 </script>
 
 <section class="mt-12 relative">
-	<div class="mt-48 flex flex-col">
+	{#each timelineItems as item, i (item.id)}
+		<div
+			use:inview={options}
+			on:inview_enter={(detail) => handleChange(detail, item)}
+			on:inview_leave={(detail) => {
+				console.log('inview_leave');
+				handleChange(detail, item);
+			}}
+		>
+			<div class="flex justify-center mx-auto relative w-auto m-auto p-4">
+				{#key item.inView}
+					<div class="absolute top-0 h-full w-2 bg-white">
+						<div class="relative m-2 {item.inView ? "block" : "hidden"}">
+							<span
+								in:maybe={{
+									inView: item.inView,
+									fn: scale,
+									duration: 500,
+									easing: quintInOut
+								}}
+								class="absolute mt-2 -left-[32px] w-[60px] h-[60px] rounded-full bg-white"
+							>
+								<div class="flex justify-center items-center w-full h-full">
+									<img
+										in:maybe={{
+											inView: item.inView,
+											fn: scale,
+											duration: 500,
+											delay: 400
+										}}
+										src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/6f/Logo_of_Twitter.svg/934px-Logo_of_Twitter.svg.png"
+										alt=""
+										class="object-contain w-[70%]"
+									/>
+								</div>
+							</span>
+						</div>
+					</div>
+				{/key}
+				<br class="mt-8 pt-8" />
+				<!-- Data -->
+				<!-- Right side -->
+				{#key item.inView}
+					<div class={item.inView && i % 2 == 0 ? 'block ml-[-300px]' : 'hidden'}>
+						<div
+							in:maybe={{
+								inView: item.inView,
+								fn: fly,
+								duration: 800,
+								x: -100,
+								easing: cubicInOut
+							}}
+							class="relative rounded-xl bg-slate-800 p-4 w-full"
+						>
+							<h1>{item.title}</h1>
+							<div>
+								<span>{item.company}</span>
+								<span>{item.date}</span>
+							</div>
+							<p class="w-full break-all prose prose-custom">
+								{@html marked(item.description)}
+							</p>
+						</div>
+					</div>
+				{/key}
+				<!-- Left Side -->
+				{#key item.inView}
+					<div class={item.inView && i % 2 != 0 ? 'block ml-[300px]' : 'hidden'}>
+						<div
+							in:maybe={{
+								inView: item.inView,
+								fn: fly,
+								duration: 800,
+								x: 500,
+								easing: cubicInOut
+							}}
+							class="relative rounded-xl bg-slate-800 p-4 w-full"
+						>
+							<h1>{item.title}</h1>
+							<div>
+								<span>{item.company}</span>
+								<span>{item.date}</span>
+							</div>
+							<p class="w-full break-all prose prose-custom">
+								{@html marked(item.description)}
+							</p>
+						</div>
+					</div>
+				{/key}
+			</div>
+		</div>
+	{/each}
+	<!-- Mobile timeline -->®®
+	<!-- <div class="mt-48 flex flex-col">
 		{#each timelineItems as item, i (item.id)}
 			<div>
 				<div
@@ -121,15 +215,5 @@
 				</div>
 			</div>
 		{/each}
-		<!-- </div>
-	<div use:inview={options} on:inview_change={handleChange}>
-		<div
-			class={isInView ? 'animate__animated animate__backInRight' : ''}
-			class:animateFromBottom={scrollDirection === 'down'}
-			class:animateFromTop={scrollDirection === 'up'}
-		>
-			Animate me!
-		</div>
 	</div> -->
-	</div>
 </section>
